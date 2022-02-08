9edb02df977f5e1ae835e5f358a15cd5948da387
MohanramAnbukkarasu <44049406+MohanramAnbukkarasu@users.noreply.github.com>
2022-02-08 13:24:30 +0530

    Add files via upload

---
+++ PivotGrid_GettingStarted/PivotGrid_GettingStarted/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Syncfusion.PivotAnalysis.Base;
+using System.Windows.Forms;
+
+namespace PivotGrid_GettingStarted
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            InitializePivotGrid();
+        }
+
+
+        private void InitializePivotGrid()
+        {
+            // Adding ItemSource to the Control
+            pivotGridControl1.ItemSource = ProductSales.GetSalesData();
+
+            // Adding PivotRows to the Control
+            pivotGridControl1.PivotRows.Add(new PivotItem { FieldMappingName = "Product", TotalHeader = "Total" });
+
+            pivotGridControl1.PivotRows.Add(new PivotItem { FieldMappingName = "Date", TotalHeader = "Total" });
+
+            // Adding PivotColumns to the Control
+            pivotGridControl1.PivotColumns.Add(new PivotItem { FieldMappingName = "Country", TotalHeader = "Total" });
+
+            pivotGridControl1.PivotColumns.Add(new PivotItem { FieldMappingName = "State", TotalHeader = "Total" });
+
+            // Adding PivotCalculations to the Control
+            pivotGridControl1.PivotCalculations.Add(new PivotComputationInfo { FieldName = "Amount", Format = "$ #,##0.00", SummaryType = SummaryType.DoubleTotalSum });
+
+            pivotGridControl1.PivotCalculations.Add(new PivotComputationInfo { FieldName = "Quantity", Format = "#,##0" });
+        }
+
+    }
+}
